870a98ec7d650b6660873e6177c37cb6b7ade227
Minte_DEV <mintesinottamene0917@gmail.com>
2025-01-28 10:47:20 +0300

    Refactor Search component to use destructuring for props and add destructuring example as a comment

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+//You added a destructuring example for an object (person) as a comment. This demonstrates destructuring, similar to how the props are handled in the Search component:
+// const person = {
+//   name: "Jon Smith",
+//   location: "London",
+//   Age: "25",
+// };
+// const { name, Age, location } = person;
+// console.log(name);  //Jon Smith
 
-function Search(props) {
-  return (
-    <div className='text-white text-3xl'>{props.searchTerm}</div>
-  )
+function Search({ searchTerm, setSearchTerm }) {
+  return <div className="text-white text-3xl">{searchTerm}</div>;
 }
 
-export default Search
+export default Search;
