9b6d4b2e745b661ad18de3b9386976f2407c2b90
Minte_DEV <mintesinottamene0917@gmail.com>
2025-02-13 09:56:29 +0300

    add MovieCard & Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/Search";
+import MovieCard from "./components/MovieCard";
+import Spinner from "./components/Spinner";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -17,6 +19,44 @@ function App() {
   const [searchTerm, setSearchTerm] = useState(""); // Define state with useState
   const [trendingMovies, setTrendingMovies] = useState([]);
 
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Function to fetch trending movies
+  const fetchTrendingMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`,
+        API_OPTIONS
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+       if(data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+
+      setTrendingMovies(data.results);
+    } catch (error) {
+      setErrorMessage(`Error fetching movies: ${error.message}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // useEffect to fetch trending movies on component mount
+  useEffect(() => {
+    fetchTrendingMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern">
@@ -27,10 +67,24 @@ function App() {
               Find <span className="text-gradient">Movies</span> You'll Enjoy
               Without the Hassle
             </h1>
-          </header>
-          {/* Pass both searchTerm and setSearchTerm as props */}
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-          {/* <h1> output of search is here this how it work <span className="text-gradient">{searchTerm}</span>            </h1> */}
+            {/* Pass both searchTerm and setSearchTerm as props */}
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>         
+        
+          <section className="all-movies">
+          <h2>All Movies</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
         </div>
       </div>
     </main>
